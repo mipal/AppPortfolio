5824a16952f723182b2ab7f203675e9c519b74ea
Michael <michael@cyanshift.com>
2015-08-14 23:29:32 +0200

    Add onClick and show toast

--- app/src/main/java/com/cyanshift/myappportfolio/MainActivity.java
+++ app/src/main/java/com/cyanshift/myappportfolio/MainActivity.java
@@ -1,9 +1,13 @@
 package com.cyanshift.myappportfolio;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +37,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void clickedButton(View view) {
+
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch " + ((Button)view).getText();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
